b69df42076834420e4c5654652e1fd8ebaace58c
LAB-ADAN <LAB_ADAN@outlook.com>
2026-05-31 19:56:50 -0400

    fix proxy port 3000 + track progress data + update gitignore

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 5173,
     strictPort: false,
     proxy: {
-      // In dev mode: forward /api calls to the Express server on 3001
+      // In dev mode: forward /api calls to the Express server on 3000
       '/api': 'http://localhost:3000',
     },
   },
